745fbf569001db495bb0679005b44cd5d1301fff
arman <gmnamra@gmail.com>
2019-05-10 09:58:09 -0700

    initial

---
+++ asift.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
+import numpy as np
+import cv2 as cv
+import imutils
+
+# built-in modules
+import itertools as it
+from multiprocessing.pool import ThreadPool
+
+# local modules
+from common import Timer
+from find_obj import init_feature, filter_matches, explore_match
+import squares
+from skimage.measure import compare_ssim
+
+def affine_skew(tilt, phi, img, mask=None):
+    '''
+    affine_skew(tilt, phi, img, mask=None) -> skew_img, skew_mask, Ai
+
+    Ai - is an affine transform matrix from skew_img to img
+    '''
+    h, w = img.shape[:2]
+    if mask is None:
+        mask = np.zeros((h, w), np.uint8)
+        mask[:] = 255
+    A = np.float32([[1, 0, 0], [0, 1, 0]])
+    if phi != 0.0:
+        phi = np.deg2rad(phi)
+        s, c = np.sin(phi), np.cos(phi)
+        A = np.float32([[c, -s], [s, c]])
+        corners = [[0, 0], [w, 0], [w, h], [0, h]]
+        tcorners = np.int32(np.dot(corners, A.T))
+        x, y, w, h = cv.boundingRect(tcorners.reshape(1, -1, 2))
+        A = np.hstack([A, [[-x], [-y]]])
+        img = cv.warpAffine(img, A, (w, h), flags=cv.INTER_LINEAR, borderMode=cv.BORDER_REPLICATE)
+    if tilt != 1.0:
+        s = 0.8 * np.sqrt(tilt * tilt - 1)
+        img = cv.GaussianBlur(img, (0, 0), sigmaX=s, sigmaY=0.01)
+        img = cv.resize(img, (0, 0), fx=1.0 / tilt, fy=1.0, interpolation=cv.INTER_NEAREST)
+        A[0] /= tilt
+    if phi != 0.0 or tilt != 1.0:
+        h, w = img.shape[:2]
+        mask = cv.warpAffine(mask, A, (w, h), flags=cv.INTER_NEAREST)
+    Ai = cv.invertAffineTransform(A)
+    return img, mask, Ai
+
+
+def affine_detect(detector, img, mask=None, pool=None):
+    '''
+    affine_detect(detector, img, mask=None, pool=None) -> keypoints, descrs
+
+    Apply a set of affine transformations to the image, detect keypoints and
+    reproject them into initial image coordinates.
+    See http://www.ipol.im/pub/algo/my_affine_sift/ for the details.
+
+    ThreadPool object may be passed to speedup the computation.
+    '''
+    params = [(1.0, 0.0)]
+    for t in 2 ** (0.5 * np.arange(1, 6)):
+        for phi in np.arange(0, 180, 72.0 / t):
+            params.append((t, phi))
+
+    def f(p):
+        t, phi = p
+        timg, tmask, Ai = affine_skew(t, phi, img)
+        keypoints, descrs = detector.detectAndCompute(timg, tmask)
+        for kp in keypoints:
+            x, y = kp.pt
+            kp.pt = tuple(np.dot(Ai, (x, y, 1)))
+        if descrs is None:
+            descrs = []
+        return keypoints, descrs
+
+    keypoints, descrs = [], []
+    if pool is None:
+        ires = it.imap(f, params)
+    else:
+        ires = pool.imap(f, params)
+
+    for i, (k, d) in enumerate(ires):
+        print('affine sampling: %d / %d\r' % (i + 1, len(params)), end='')
+        keypoints.extend(k)
+        descrs.extend(d)
+
+    print()
+    return keypoints, np.array(descrs)
+
+
+if __name__ == '__main__':
+    print(__doc__)
+
+    import sys, getopt
+
+    opts, args = getopt.getopt(sys.argv[1:], '', ['feature='])
+    opts = dict(opts)
+    feature_name = opts.get('--feature', 'sift')
+    try:
+        fn1, fn2 = args
+    except:
+        fn1 = 'images/TI01.png'
+        fn2 = 'images/TI02.png'
+
+    img1 = cv.imread(fn1, 0)
+    img2 = cv.imread(fn2, 0)
+    rows, cols = map(int, img1.shape)
+
+    img1 = cv.pyrDown(img1, dstsize=(cols // 2, rows // 2))
+    img2 = cv.pyrDown(img2, dstsize=(cols // 2, rows // 2))
+    cols = cols // 2
+    rows = rows // 2
+
+    img1 = cv.pyrDown(img1, dstsize=(cols // 2, rows // 2))
+    img2 = cv.pyrDown(img2, dstsize=(cols // 2, rows // 2))
+    cols = cols // 2
+    rows = rows // 2
+
+    sq1 = squares.find_squares(img1)
+
+    sq2 = squares.find_squares(img2)
+
+    detector, matcher = init_feature(feature_name)
+
+    if img1 is None:
+        print('Failed to load fn1:', fn1)
+        sys.exit(1)
+
+    if img2 is None:
+        print('Failed to load fn2:', fn2)
+        sys.exit(1)
+
+    if detector is None:
+        print('unknown feature:', feature_name)
+        sys.exit(1)
+
+    print('using', feature_name)
+
+    pool = ThreadPool(processes=cv.getNumberOfCPUs())
+    kp1, desc1 = affine_detect(detector, img1, pool=pool)
+    kp2, desc2 = affine_detect(detector, img2, pool=pool)
+    print('img1 - %d features, img2 - %d features' % (len(kp1), len(kp2)))
+
+    img_matches = np.empty((max(img1.shape[0], img1.shape[0]), img1.shape[1] + img2.shape[1], 3), dtype=np.uint8)
+
+
+    def match_and_draw(win):
+        with Timer('matching'):
+            raw_matches = matcher.knnMatch(desc1, trainDescriptors=desc2, k=2)  # 2
+        p1, p2, kp_pairs = filter_matches(kp1, kp2, raw_matches)
+
+        cv.drawMatches(img1, kp1, img2, kp2, [], img_matches, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+        H = None
+        if len(p1) >= 4:
+            H, status = cv.findHomography(p1, p2, cv.RANSAC, 5.0)
+            print('%d / %d  inliers/matched' % (np.sum(status), len(status)))
+
+            for s1 in sq1:
+                obj_corners = np.empty((4, 1, 2), dtype=np.float32)
+                obj_corners[0, 0, 0] = s1[0][0]
+                obj_corners[0, 0, 1] = s1[0][1]
+                obj_corners[1, 0, 0] = s1[1][0]
+                obj_corners[1, 0, 1] = s1[1][1]
+                obj_corners[2, 0, 0] = s1[2][0]
+                obj_corners[2, 0, 1] = s1[2][1]
+                obj_corners[3, 0, 0] = s1[3][0]
+                obj_corners[3, 0, 1] = s1[3][1]
+
+                scene_corners = cv.perspectiveTransform(obj_corners, H)
+
+                # -- Draw lines between the corners (the mapped object in the scene - image_2 )
+                cv.line(img_matches, (int(scene_corners[0, 0, 0] + img1.shape[1]), int(scene_corners[0, 0, 1])), \
+                        (int(scene_corners[1, 0, 0] + img1.shape[1]), int(scene_corners[1, 0, 1])), (0, 255, 0), 4)
+                cv.line(img_matches, (int(scene_corners[1, 0, 0] + img1.shape[1]), int(scene_corners[1, 0, 1])), \
+                        (int(scene_corners[2, 0, 0] + img1.shape[1]), int(scene_corners[2, 0, 1])), (0, 255, 0), 4)
+                cv.line(img_matches, (int(scene_corners[2, 0, 0] + img1.shape[1]), int(scene_corners[2, 0, 1])), \
+                        (int(scene_corners[3, 0, 0] + img1.shape[1]), int(scene_corners[3, 0, 1])), (0, 255, 0), 4)
+                cv.line(img_matches, (int(scene_corners[3, 0, 0] + img1.shape[1]), int(scene_corners[3, 0, 1])), \
+                        (int(scene_corners[0, 0, 0] + img1.shape[1]), int(scene_corners[0, 0, 1])), (0, 255, 0), 4)
+
+                cv.line(img_matches, (int(obj_corners[0, 0, 0]), int(obj_corners[0, 0, 1])), \
+                        (int(obj_corners[1, 0, 0]), int(obj_corners[1, 0, 1])), (255, 255, 0), 4)
+                cv.line(img_matches, (int(obj_corners[1, 0, 0]), int(obj_corners[1, 0, 1])), \
+                        (int(obj_corners[2, 0, 0]), int(obj_corners[2, 0, 1])), (255, 255, 0), 4)
+                cv.line(img_matches, (int(obj_corners[2, 0, 0]), int(obj_corners[2, 0, 1])), \
+                        (int(obj_corners[3, 0, 0]), int(obj_corners[3, 0, 1])), (255, 255, 0), 4)
+                cv.line(img_matches, (int(obj_corners[3, 0, 0]), int(obj_corners[3, 0, 1])), \
+                        (int(obj_corners[0, 0, 0]), int(obj_corners[0, 0, 1])), (255, 255, 0), 4)
+
+                # -- Show detected matches
+                cv.imshow('Good Matches & Object detection', img_matches)
+                cv.waitKey()
+
+            return H
+
+    HH = match_and_draw('affine find_obj')
+    img1_aligned = cv.warpPerspective(img1, HH, (cols, rows), flags=cv.INTER_LINEAR + cv.WARP_INVERSE_MAP)
+    img2_aligned = cv.warpPerspective(img2, HH, (cols, rows), flags=cv.INTER_LINEAR + cv.WARP_INVERSE_MAP)
+
+    motion = None
+    motion = cv.absdiff(img2_aligned, img1)
+    diff = motion.copy()
+    cv.threshold(motion, 80, 255, cv.THRESH_BINARY)
+    cnts = cv.findContours(motion, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    for c in cnts:
+        # compute the bounding box of the contour and then draw the
+        # bounding box on both input images to represent where the two
+        # images differ
+        (x, y, w, h) = cv.boundingRect(c)
+        cv.rectangle(img1, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv.rectangle(img2_aligned, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # show the output images
+
+    cv.imshow("TOI01", img1)
+    cv.imshow("TOI01", img2)
+    cv.imshow("Aligned Image 1", img1_aligned)
+    cv.imshow("Aligned Image 2", img2_aligned)
+    cv.imshow("Difference", motion)
+    cv.imshow("raw diff", diff)
+
+
+
+    cv.waitKey()
+
+    cv.destroyAllWindows()
---
+++ sobelplus.py
@@ -0,0 +1,157 @@
+import sys
+from pathlib import Path
+
+import cv2
+import numpy as np
+import opencv_utils
+from matplotlib import pyplot as plt
+from skimage import draw
+from skimage import img_as_float
+from skimage import img_as_ubyte
+from skimage import measure
+import squares
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Polygon
+
+bins = [  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,
+        13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,
+        26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,
+        39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,
+        52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,  64,
+        65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,
+        78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,
+        91,  92,  93,  94,  95,  96,  97,  98,  99, 100, 101, 102, 103,
+       104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
+       117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129,
+       130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142,
+       143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155,
+       156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168,
+       169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181,
+       182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194,
+       195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
+       208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220,
+       221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233,
+       234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246,
+       247, 248, 249, 250, 251, 252, 253, 254, 255]
+
+def axis(x): return int((x + 16) // 32) % 8
+
+
+def truePeaks(magnitudes, phases, isUnique=None):
+    tps = []
+    axh = np.zeros(256, np.int32)
+    ltps = [[], [], [], [], [], [], [], []]
+    offsets = [[[0, -1], [0, 1]], [[-1, -1], [1, 1]], [[-1, 0], [1, 0]], [[-1, 1], [1, -1]]]
+
+    height, width = magnitudes.shape[:2]
+    for i in range(2, height - 2, 1):
+        for j in range(2, width - 2, 1):
+            angle = phases[i, j]
+            mag = magnitudes[i, j]
+            if mag < 30: continue
+            ax = axis(angle)
+            axh[angle] += 1
+            m1 = m2 = mag
+            if ax == 0 or ax == 4:  # left & right column
+                m1 = magnitudes[i, j - 1]
+                m2 = magnitudes[i, j + 1]
+            elif ax == 1 or ax == 5:  # top left & bottom right
+                m1 = magnitudes[i - 1, j - 1]
+                m2 = magnitudes[i + 1, j + 1]
+            elif ax == 2 or ax == 6:  # top & bottom row
+                m1 = magnitudes[i - 1, j]
+                m2 = magnitudes[i + 1, j]
+            elif ax == 3 or ax == 7:  # bottom left and top right
+                m1 = magnitudes[i - 1, j + 1]
+                m2 = magnitudes[i + 1, j - 1]
+            else:
+                assert (False)
+
+            if ((mag > m1 and mag >= m2) or (mag >= m1 and mag > m2)):
+                #                if not (isUnique is None) and isTpUnique ( i, j, ax ):
+                ltps[ax].append([j, i, ax, angle, mag])
+
+    tps = np.concatenate(ltps)
+
+    return (tps,axh)
+
+
+
+def process(img, half_size=1):
+    hs = half_size - int(half_size) % 1
+    block_size = 2 * hs + 1
+
+    sobelx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=block_size)
+    sobely = cv2.Sobel(img, cv2.CV_32F, 0, 1, ksize=block_size)
+    phases = cv2.phase(sobelx, sobely, False)
+    #    phases = np.unwrap(phases, np.pi + np.pi) / (2 * np.pi)
+    phases = np.divide(phases, np.pi + np.pi)
+    phases = img_as_ubyte(phases)
+    magnitudes = np.sqrt(sobelx ** 2 + sobely ** 2)
+
+    return (magnitudes, phases)
+
+
+if __name__ == '__main__':
+    result = None
+    display = None
+    tcontours = None
+    contours = None
+
+
+    def data_circle():
+        arr = np.zeros((200, 200))
+        rr, cc = draw.circle(100, 100, 67)
+        arr[rr, cc] = 200
+        img = cv2.GaussianBlur(arr, (7, 7), sigmaX=1.2)
+        return img
+
+
+    contours = None
+    corners = None
+
+    if len(sys.argv) < 2:
+        # Construct some test data
+        r = img_as_float(data_circle())
+        result = process(np.uint8(r), 1)
+        contours = measure.find_contours(r, 0.5)
+        display = r
+    else:
+        if not Path(sys.argv[1]).is_file() or not Path(sys.argv[1]).exists():
+            print(sys.argv[1] + '  Does not exist ')
+
+        lab_tuple = opencv_utils.load_reduce_convert(sys.argv[1], 3)
+        corners = squares.find_squares(lab_tuple[0])
+        display = opencv_utils.convert_lab2rgb(lab_tuple)
+        result = process(lab_tuple[0], 1)
+
+    tps, axh = truePeaks(result[0], result[1])
+    truepeaks = np.array(tps, dtype='f')
+
+
+    fig = plt.figure(figsize=(22, 15))
+
+
+
+    ax2 = plt.subplot2grid((3, 3), (0, 0))
+    ax2.imshow(result[0], cmap='gray')
+    ax2.set_title('Gradient Magnitude')
+
+    ax2 = plt.subplot2grid((3, 3), (0, 1))
+    ax2.imshow(result[1], cmap='gray')
+    ax2.set_title('Gradient Angle')
+
+    ax1 = plt.subplot2grid((3, 3), (1, 0))
+    ax1.imshow(display, cmap='gray')
+    ax1.plot(truepeaks[:, 0], truepeaks[:, 1], '+r', markersize=3)
+    ax1.set_title('Image + TP ')
+
+    # Display histogram
+    ax_hist = plt.subplot2grid((3, 3), (1, 1))
+    ax_hist.hist(result[1].ravel(), bins=256,density=True)
+    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+    ax_hist.set_xlabel(' Direction / 2 PI')
+  #  ax_hist.set_xlim(0, 1.0)
+   # ax_hist.set_yticks([])
+
+    plt.show()
