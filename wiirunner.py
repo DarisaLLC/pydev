1b0f1d0134b6c25b2b10472fcb848f72bc99ad4b
gmnamra <gmnamra@gmail.com>
2020-01-26 18:23:39 -0800

    adjusted farnback motion parameters

--- wiirunner.py
+++ wiirunner.py
@@ -1,21 +1,30 @@
 import sys
+import os
 from pathlib import Path
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2 as cv
 import math
+from padChecker import padChecker
 from wiitricity_settings import video_rois, initialize_settings_from_video_roi, initialize_settings
 from wiitricity_settings import region_of_interest, vertical, _draw_str, get_logger, get_line_angle
-from wiitricity_settings import circular_mean
+from wiitricity_settings import circular_mean, line_yhist, compute_lines, quadrilateral_detection
+from eval_homography import image_change
+from skimage.feature import peak_local_max
+from skimage.util import img_as_float, img_as_ubyte
 
+
+import numpy as np
 
 import logging
 from datetime import datetime
 
+
 class gpad_odometry:
 
-    def __init__(self, video_source_info, user_roi ):
+    def __init__(self, video_source_info, user_roi, data_path):
         self.source_info = video_source_info
+        self.data_cache = data_path
         self.source = None
         if self.source_info == '0' or self.source_info == '1':
             self.source = 'Camera'
@@ -34,10 +43,14 @@ class gpad_odometry:
         self.frame_idx = 0
         self.prev_gray_frame = None
         self.prev_frame = None
+        self.abs_diff = None
         self.prev_time = None
         self.current_angle = None
         self.current_distance = None
         self.current_time = None
+        self.prev_angle = None
+        self.prev_distance = None
+        self.prev_time = None
         self.flow_mask = None
         self.mask = None
         self.cycles = 0
@@ -46,8 +59,8 @@ class gpad_odometry:
         self.logger.info('Source is ' + self.source)
         video_roi = user_roi
         if self.source == 'Camera':
-            camera_roi = dict(row_low=0, row_high=int(self.cap.get(cv.CAP_PROP_FRAME_HEIGHT))-1,
-                                column_low=0, column_high=int(self.cap.get(cv.CAP_PROP_FRAME_WIDTH))-1)
+            camera_roi = dict(row_low=0, row_high=int(self.cap.get(cv.CAP_PROP_FRAME_HEIGHT)) - 1,
+                              column_low=0, column_high=int(self.cap.get(cv.CAP_PROP_FRAME_WIDTH)) - 1)
             video_roi = camera_roi
 
         self.logger.info(video_roi)
@@ -74,7 +87,18 @@ class gpad_odometry:
         self.direction = []
         self.avg_angle = None
         self.show_vectors = False
-        self.show_axis = False
+        self.show_pads = False
+        self.show_ga_score = True
+
+        # Get a GA pad checker object
+        self.checker = padChecker(self.data_cache)
+        self.ga_results = None
+        self.prev_ga_results = None
+        self.prev_lines = None
+        self.lines = None
+
+    def distance_is_available(self):
+        return not (self.current_distance is None)
 
     def is_loaded(self):
         return self._is_loaded
@@ -89,10 +113,14 @@ class gpad_odometry:
 
             gray_frame, frame = self.prepare_frame(captured_frame)
             self.get_flow(gray_frame)
+            self.line_processing(gray_frame)
+            #     self.display = self.detect_ga(frame)
             self.prev_frame = frame.copy()
             self.prev_gray_frame = gray_frame.copy()
             self.frame_idx = self.frame_idx + 1
 
+            self.draw_direction()
+            self.draw_frame_info()
             cv.namedWindow('Display', cv.WINDOW_NORMAL)
             cv.imshow("Display", self.display)
             ch = cv.waitKey(1)
@@ -107,22 +135,110 @@ class gpad_odometry:
         self.flow_mask = vertical(frame)
         self.display = frame.copy()
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # filename = '/Users/arman/tmpin/' + str(self.frame_idx) + '.png'
+        # cv.imwrite(filename, frame)
         return gray_frame, frame
+
+    def line_processing(self, gray_frame):
+        half_width = self.width//2
+        half_height = self.height//2
+
+        topleft = (half_width//8, half_height//4)
+        botright = half_width - topleft[0], half_height - topleft[1]
+        lines_valid_region = (topleft, botright)
+        lines_mask_region = [(0,0),(half_width-1, half_height//3)]
+        half_res = cv.resize(gray_frame,(half_width,half_height))
+        half_res = cv.GaussianBlur(half_res, (0,0), 1)
+
+        (lines, locations, directions, strengths, kde) = compute_lines(half_res, lines_valid_region,(15, 100))
+        print((len(locations), ' Lines '))
+       # parallels = quadrilateral_detection(lines, locations, directions, strengths, kde, (15,100))
+
+
+        if self.prev_gray_frame is None:
+            self.prev_gray_frame = half_res
+            self.prev_lines = lines
+        else:
+            self.prev_lines = self.lines
+            self.lines = lines
+
+        def bgrFromHue(degrees):
+            hsv = np.zeros((1, 1, 3), np.uint8)
+            hsv[0, 0, 0] = ((degrees % 360) * 255) / 360.0
+            hsv[0, 0, 1] = 255
+            hsv[0, 0, 2] = 255
+            bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+            tp = tuple([int(x) for x in bgr[0, 0, :]])
+            return tp
+
+        # if not (self.prev_lines is None):
+        #     for line in self.prev_lines:
+        #         x1, y1, x2, y2 = line
+        #         cv.line(self.display, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+        vornoi = np.zeros((self.height, self.width, 1), dtype="uint8")
+        vornoi += 255
+
+        if not (self.lines is None):
+            for line in self.lines:
+                x1, y1, x2, y2 = line
+                radians = np.arctan2(y2 - y1, x2 - x1)
+                cl = bgrFromHue(math.degrees(radians))
+                cv.line(self.display, (x1+x1, y1+y1), (x2+x2, y2+y2), cl, 2)
+                cv.line(vornoi, (x1+x1, y1+y1), (x2+x2, y2+y2), (0,0,0), 2)
+
+        dist_transform = cv.distanceTransform(vornoi, cv.DIST_L2, 5, None, cv.CV_32F)
+
+        ## Find all local maximas in the distance image
+        peaks = peak_local_max(img_as_float(dist_transform),min_distance=50, indices=False)
+        peaks_img = img_as_ubyte(peaks)
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                # threshold the pixel
+                if peaks_img[y,x]:
+                    cv.circle(self.display, (x,y), 30, (0,0,255), 5)
+
+        cv.imwrite('/Users/arman/vtmp/' + str(self.frame_idx) + '.png', dist_transform)
+        cv.imwrite('/Users/arman/vvtmp/' + str(self.frame_idx) + '.png', vornoi)
+        cv.imwrite('/Users/arman/vvvtmp/' + str(self.frame_idx) + '.png', peaks_img)
+
+        # dist2 = cv.normalize(dist_transform, None, 255, 0, cv.NORM_MINMAX, cv.CV_8UC1)
+        # cv.namedWindow('Display', cv.WINDOW_NORMAL)
+        # cv.imshow("Display", dist2)
+        # ch = cv.waitKey(10)
+
+
+    def detect_ga(self, frame):
+        res = self.display
+        #    if self.distance_is_available() and int(self.current_distance) < 5:
+        # readout,thresh, seethrough, cnts, hulls
+        checkout = self.checker.check(frame, self.mask)
+        if self.ga_results is None:
+            self.ga_results = checkout
+        else:
+            self.prev_ga_result = self.ga_results
+            self.ga_results = checkout
+
+        if not (self.ga_results is None):
+            score = str(self.ga_results[0])
+            _draw_str(self.display, (200, 400), score, 2.0)
+            #  res = cv.drawContours(res, self.ga_results[4], -1, (255, 0, 0), 3)
+            if self.show_pads:
+                res = np.vstack((res, self.ga_results[2]))
+        return res
 
     def get_roi(self, frame):
         assert (self._is_valid)
         return frame[self.row_range[0]:self.row_range[1], self.column_range[0]:self.column_range[1]]
 
-
-
-
     def get_flow(self, frame_gray):
         if len(self.tracks) > 0:
             img0, img1 = self.prev_gray_frame, frame_gray
+
             p0 = np.float32([tr[-1] for tr in self.tracks]).reshape(-1, 1, 2)
             p1, _st, _err = cv.calcOpticalFlowPyrLK(img0, img1, p0, None, **self.settings['lk_params'])
             p0r, _st, _err = cv.calcOpticalFlowPyrLK(img1, img0, p1, None, **self.settings['lk_params'])
-            d = abs(p0-p0r).reshape(-1, 2).max(-1)
+            d = abs(p0 - p0r).reshape(-1, 2).max(-1)
             good = d < 1
             new_tracks = []
             for tr, (x, y), good_flag in zip(self.tracks, p1.reshape(-1, 2), good):
@@ -152,29 +268,35 @@ class gpad_odometry:
             mean_angle = circular_mean(mags, angles)
             if mean_angle < 0:
                 mean_angle = math.pi + math.pi + mean_angle
-            mean_angle = mean_angle % (2*math.pi)
+            mean_angle = mean_angle % (2 * math.pi)
             mean_mags = np.mean(mags)
-            self.current_distance = mean_mags
-            if self.current_distance > 0:
-                self.current_angle = mean_angle
 
-            _draw_str(self.display, (200, 200), 'Mean Direction: %d' % math.degrees(self.current_angle), 2.0)
-            _draw_str(self.display, (200, 250), 'Mean Distance: %d' % mean_mags, 2.0)
+            if self.prev_angle is None:
+                self.prev_angle = mean_angle
+            if self.prev_distance is None:
+                self.prev_distance = mean_mags
+
+            self.prev_distance = self.current_distance
+            self.current_distance = mean_mags
+
+            if int(self.current_distance) > 0:
+                self.prev_angle = self.current_angle
+                self.current_angle = mean_angle
+            else:
+                self.current_angle = self.prev_angle
 
             if self.show_vectors:
                 cv.polylines(self.display, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
-            _draw_str(self.display, (20, self.height-100), ' %d' % len(self.tracks), 2.0)
+                _draw_str(self.display, (20, self.height - 100), ' %d' % len(self.tracks), 2.0)
 
         if self.frame_idx % self.detect_interval == 0:
             mask = self.flow_mask.copy()
             for x, y in [np.int32(tr[-1]) for tr in self.tracks]:
                 cv.circle(mask, (x, y), 5, 0, -1)
-            p = cv.goodFeaturesToTrack(frame_gray, mask = mask, **self.settings['feature_params'])
+            p = cv.goodFeaturesToTrack(frame_gray, mask=mask, **self.settings['feature_params'])
             if p is not None:
                 for x, y in np.float32(p).reshape(-1, 2):
                     self.tracks.append([(x, y)])
-
-
 
     def get_mean_distance_2d(self, features1, features2):
         num_features = min((len(features1), len(features2)))
@@ -198,24 +320,14 @@ class gpad_odometry:
         return dist
 
     def draw_direction(self):
-        if self.prev_state != State.eUnknown and self.current_state != State.eUnknown and self.cycles > 1:
-            direction = self.angle_states[self.current_state]
-            cv.putText(self.display, direction, (self.width // 2, self.height // 2), cv.FONT_HERSHEY_DUPLEX,
-                       2, (225, 0, 0), 7)
+        if not (self.current_angle is None):
+            _draw_str(self.display, (self.width // 8, (15 * self.height) // 16), 'Mean Direction: %d' % math.degrees(self.current_angle), 2.0)
+        if not (self.current_distance is None):
+            _draw_str(self.display, ((2*self.width) // 3, (15 * self.height) // 16), 'Mean Distance: %d' % self.current_distance, 2.0)
 
-    def draw_tracks(self, points1, points2):
-        if self.show_vectors:
-            for i, (new, old) in enumerate(zip(points1, points2)):
-                a, b = new.ravel()
-                c, d = old.ravel()
-                angle = get_line_angle([c, d, a, b])
-                cl = bgrFromHue(math.degrees(angle))
-                if a < self.width / 2: cl = (255,0,0)
-                frame = cv.line(self.display, (c, d), (a, b), cl, 1)
-                cv.circle(self.display, (c,d), 5, cl, 0, 3)
-
-        self.draw_direction()
-        cv.putText(self.display, str(self.frame_idx), (self.width // 16, (15*self.height) // 16), cv.FONT_HERSHEY_SIMPLEX,
+    def draw_frame_info(self):
+        cv.putText(self.display, str(self.frame_idx), (self.width // 16, (15 * self.height) // 16),
+                   cv.FONT_HERSHEY_SIMPLEX,
                    1, (0, 255, 0), 2)
 
     def measure_tracks(self):
@@ -242,20 +354,37 @@ class gpad_odometry:
         return mean_angle % (2 * math.pi)
 
 
-
-
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+
+    if len(sys.argv) < 1:
+        sys.exit(1)
+
+    default_data_path = os.path.dirname(os.path.realpath(__file__)) + '/projects/wiic/'
+    default_is_good = Path(default_data_path).exists() and Path(default_data_path).is_dir()
+    msg = ''
+    if len(sys.argv) == 2 and default_is_good:
+        data_path = default_data_path
+        print(('Default Data Directory is used ', default_data_path))
+    elif len(sys.argv) == 3 and Path(sys.argv[2]).exists() and Path(sys.argv[2]).is_dir():
+        data_path = sys.argv[2]
+        print(('User Supplied Data Directory is used ', data_path))
+    elif len(sys.argv) == 3 and default_is_good:
+        data_path = default_data_path
+        print(('User Supplied is Invalid: Default Data Directory is used ', default_data_path))
+    else:
+        print(('User Supplied Data Directory does not exist or is Invalid ', ' Default Data Directory does not exist '))
         sys.exit(1)
 
     # use video_roi['hd'] for newer video
-    runner = gpad_odometry(sys.argv[1], video_rois['hd'])
+    runner = gpad_odometry(sys.argv[1], video_rois['hd'], data_path)
     loaded = runner.is_loaded()
     if not loaded:
         self.logger.info('Video Did not Load')
         exit(1)
     runner.run()
 
-#  cv.waitKey(0)
-#  cv.destroyAllWindows()
+    cv.destroyAllWindows()
+
 #  cap.release()
+#  cv.waitKey(0)
+#
--- wiitricity_settings.py
+++ wiitricity_settings.py
@@ -1,18 +1,15 @@
 import numpy as np
 import sys
+import os
 import math
 import cv2 as cv
 import datetime
 import logging
 from datetime import datetime
-from enum import Enum, unique
-
-@unique
-class State(Enum):
-    eRight=-1,
-    eLeft=+1,
-    eSame=0,
-    eUnknown=-2
+from pathlib import Path
+from sympy import simplify, Polygon, convex_hull, Point2D, Line2D, Line
+import itertools
+from simple_kdtree import make_kd_tree, add_point, get_knn, get_nearest, PointContainer
 
 '''
 Initialization of common settings for WiiTricity image & videos
@@ -22,46 +19,46 @@ frame_tl is the topleft corner of image data in the stored image or video frame
 capture_size is the image size 
 
 '''
-video_rois = {'one' : dict(row_low=53,row_high=350,column_low=6,column_high=708),
-              'hd2' : dict(row_low=0,row_high=759,column_low=0,column_high=1919),
-              'hd' : dict(row_low=0,row_high=519,column_low=0,column_high=1279)}
+video_rois = {'one': dict(row_low=53, row_high=350, column_low=6, column_high=708),
+              'hd2': dict(row_low=0, row_high=759, column_low=0, column_high=1919),
+              'hd': dict(row_low=0, row_high=519, column_low=0, column_high=1279)}
+
 
 def initialize_settings_from_video_roi(video_roi):
     frame_tl = (video_roi['column_low'], video_roi['row_low'])
-    capture_size = (video_roi['column_high']-video_roi['column_low'],video_roi['row_high']-video_roi['row_low'])
+    capture_size = (video_roi['column_high'] - video_roi['column_low'], video_roi['row_high'] - video_roi['row_low'])
     return initialize_settings(frame_tl, capture_size)
+
 
 def initialize_settings(frame_tl, capture_size):
     settings = {'frameTopLeft': frame_tl, 'active_center_norm': (0.5, 0.5), 'active_radius_norm': 0.4,
-                'capture_size': capture_size, 'cache':'.'}
+                'capture_size': capture_size, 'cache': '.'}
 
     width = int(settings['capture_size'][0] + 0.5) - settings['frameTopLeft'][0] * 2
     height = int(settings['capture_size'][1] + 0.5) - settings['frameTopLeft'][1] * 2
     settings['frame_size'] = (width, height)
     settings['frameBottomRight'] = (frame_tl[0] + width, frame_tl[1] + height)
     settings['active_center'] = (int(width * settings['active_center_norm'][0]),
-                                  int(height * settings['active_center_norm'][1]))
+                                 int(height * settings['active_center_norm'][1]))
 
     settings['active_radius'] = int(height * settings['active_radius_norm'])
     settings['frame_count'] = 0
     settings['ppMedianBlur'] = 7
     # Parameters for lucas kanade optical flow
     settings['lk_params'] = dict(winSize=(15, 15),
-                     maxLevel=2,
-                     criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+                                 maxLevel=3,
+                                 criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
     # params for ShiTomasi corner detection
     settings['feature_params'] = dict(maxCorners=500,
-                          qualityLevel=0.3,
-                          minDistance=7,
-                          blockSize=7)
-#                          useHarrisDetector=False,
-#                          k=0.04)
+                                      qualityLevel=0.3,
+                                      minDistance=7,
+                                      blockSize=7)
+    #                          useHarrisDetector=False,
+    #                          k=0.04)
     settings['max_distance'] = 5
     settings['min_features'] = 300
     settings['mask_diagonal_ratio'] = 2.5
-
-
 
     return settings
 
@@ -107,7 +104,7 @@ def vertical(img, top_portion=0.667):
     top = int(rows * top_portion)
 
     poly = np.array([[
-        (0, top), (cols-1, top), (cols - 1, rows - 1), (0, rows - 1)]], np.int32)
+        (0, top), (cols - 1, top), (cols - 1, rows - 1), (0, rows - 1)]], np.int32)
 
     cv.fillConvexPoly(mask, poly, 255)
     return mask
@@ -115,28 +112,40 @@ def vertical(img, top_portion=0.667):
 
 def _draw_str(dst, target, s, scale=1.0):
     x, y = target
-    cv.putText(dst, s, (x+1, y+1), cv.FONT_HERSHEY_PLAIN, scale, (0, 255, 0), thickness = 2, lineType=cv.LINE_AA)
-    cv.putText(dst, s, (x, y), cv.FONT_HERSHEY_PLAIN, scale, (128,128,128), lineType=cv.LINE_AA)
+    cv.putText(dst, s, (x + 1, y + 1), cv.FONT_HERSHEY_PLAIN, scale, (0, 255, 0), thickness=2, lineType=cv.LINE_AA)
+    cv.putText(dst, s, (x, y), cv.FONT_HERSHEY_PLAIN, scale, (128, 128, 128), lineType=cv.LINE_AA)
+
+
+def create_logging_directory(output_path):
+    client_output = output_path and Path(output_path).exists() and Path(output_path).is_dir()
+    if not client_output and Path(output_path).parent.exists:
+        os.mkdir(output_path)
+    return Path(output_path).exists
 
 
 # create logger with 'wiirunner'
 def get_logger():
-    logfilename = 'wiirunner'+datetime.now().strftime("-%d-%m-%Y_%I-%M-%S_%p")+'.log'
+    # create a logger and setup console logger
     logger = logging.getLogger('wiirunner')
     logger.setLevel(logging.DEBUG)
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler(logfilename)
-    fh.setLevel(logging.DEBUG)
-    # create console handler with a higher log level
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
     ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(fh)
     logger.addHandler(ch)
+
+    # create file logger if we can create the logging directory in logs sub directory under code
+    logging_folder = os.path.dirname(os.path.realpath(__file__)) + '/logs'
+    dir_good = create_logging_directory(logging_folder)
+    if dir_good:
+        logfilepath = logging_folder + '/wiirunner' + datetime.now().strftime("-%d-%m-%Y_%I-%M-%S_%p") + '.log'
+        # create file handler which logs even debug messages
+        fh = logging.FileHandler(logfilepath)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
     return logger
 
 
@@ -149,10 +158,12 @@ def bgrFromHue(degrees):
     tp = tuple([int(x) for x in bgr[0, 0, :]])
     return tp
 
+
 def get_line_angle(line):
     x1, y1, x2, y2 = np.array(line, dtype=np.float64)
     radians = np.arctan2(y2 - y1, x2 - x1)
     return radians
+
 
 def circular_mean(weights, angles):
     x = y = 0.
@@ -162,3 +173,209 @@ def circular_mean(weights, angles):
 
     mean = math.degrees(math.atan2(y, x))
     return mean
+
+
+'''
+mask region is a rectangle in tl image system
+represented by tl and br coords
+
+'''
+
+
+def compute_lines(image, mask_region, length_limit):
+    # Create LSD detector with default parameters
+    '''
+    cv2.LSD_REFINE_STD ,0.97, 0.6, 0.8, 40, 0, 0.90, 1024
+    '''
+    lsd = cv.createLineSegmentDetector(0) #cv.LSD_REFINE_STD, 0.8, 0.6, 2.0, 22.5, 0, 0.9, 1024)
+
+    # Detect lines in the image
+    # Returns a NumPy array of type N x 1 x 4 of float32
+    # such that the 4 numbers in the last dimension are (x1, y1, x2, y2)
+    # These denote the start and end positions of a line
+    lines = lsd.detect(image)[0]
+
+    # Remove singleton dimension
+    lines = lines[:, 0]
+
+    # Filter out the lines whose length is lower than the threshold
+    dx = lines[:, 2] - lines[:, 0]
+    dy = lines[:, 3] - lines[:, 1]
+    lengths = np.sqrt(dx * dx + dy * dy)
+    mask = lengths > length_limit[0]
+    lines = lines[mask]
+
+    locations = []
+    strengths = []
+    tl, br = mask_region
+    new_lines = []
+    new_directions = []
+    points = []
+
+    for line in lines:
+        p0, p1 = np.array([line[0], line[1]]), np.array([line[2], line[3]])
+        midp = (p0 + p1) / 2
+        within = midp[0] >= tl[0] and midp[0] < br[0] and midp[1] >= tl[1] and midp[1] < br[1]
+        if not within: continue
+        locations.append((p0 + p1) / 2)
+        strengths.append(np.linalg.norm(p1 - p0))
+        new_lines.append(line)
+        points.append(PointContainer(p0, name=len(points)))
+        points.append(PointContainer(p1, name=len(points)))
+
+    kdt = make_kd_tree(points, 2)
+    # convert to numpy arrays and normalize
+    locations = np.array(locations)
+    strengths = np.array(strengths)
+    new_lines = np.array(new_lines)
+    directions = np.arctan2(new_lines[:, 3] - new_lines[:, 1], new_lines[:, 2] - new_lines[:, 0])
+
+
+
+    return (new_lines, locations, directions, strengths, kdt)
+
+
+def angle_diff(angle1: float, angle2: float) -> float:
+    """Computes the angle difference between the input arguments.
+
+    .. note:: The resulting angle difference is in [0, pi * 0.5]
+
+    :param angle1: First angle expressed in radiants.
+    :param angle2: Second angle expressed in radiants.
+    :return: Angle difference between the input parameters. This angle represents the smallest positive angle between the input parameters.
+    """
+    d_angle = np.abs(angle1 - angle2)
+    d_angle = d_angle % np.pi
+    if d_angle > np.pi * 0.5:
+        d_angle -= np.pi
+    return np.abs(d_angle)
+
+
+def midPoint(line):
+    p0, p1 = np.array([line[0], line[1]]), np.array([line[2], line[3]])
+    return np.array((p0 + p1) / 2)
+
+
+from collections.abc import Hashable
+
+
+def uniques(iterable):
+    seen_hashable = set()
+    seen_unhashable = []
+    for item in iterable:
+        if isinstance(item, Hashable):
+            if item not in seen_hashable:
+                yield item
+                seen_hashable.add(item)
+        else:
+            if item not in seen_unhashable:
+                yield item
+                seen_unhashable.append(item)
+
+
+def quadrilateral_detection(lines, locations, directions, strengths, kdt, line_limits=(50, 100), parallel_thr=5.0):
+    cands = []
+    N = len(lines)
+    parallels = []
+    dim = 2
+
+    def dist_sq(a, b, dim):
+        return sum((a[i] - b[i]) ** 2 for i in range(dim))
+
+    def dist_sq_dim(a, b):
+        return dist_sq(a, b, dim)
+
+    iterator = itertools.combinations(range(N), 2)
+    for i, j in iterator:
+        dD = angle_diff(directions[i], directions[j])
+        if math.degrees(dD) > parallel_thr: continue
+        mid_i = locations[i]
+        mid_j = locations[j]
+        midp = midPoint([mid_i[0], mid_i[1], mid_j[0], mid_j[1]])
+        nknn = get_knn(kdt, midp, 8, dim, dist_sq_dim)
+        distances = []
+        lids = []
+        for nn in nknn:
+            distances.append(nn[0])
+            lids.append(nn[1].name)
+        ulids = uniques(lids)
+        lulids = len(list(ulids))
+        if lulids == 8: continue
+        print(len(list(ulids)), ' Uniques')
+        #
+        # dis = np.linalg.norm(mid_i - mid_j)
+        # if dis < line_limits[0] and dis < line_limits[1]:
+        #     parallels.append([mid_i[0],mid_i[1],mid_j[0],mid_j[1]])
+
+    return parallels
+
+
+'''
+ Lines are already been pruned to same angle 
+ We are looking for quadrilaterals
+
+ '''
+
+
+def line_yhist(lines, frame_size):
+    # create a y histogram of lines
+    #   la = np.array(lines, dtype='float').view(np.recarray)
+    # Remove singleton dimension
+    la = lines[:, 0]
+
+    radians = np.arctan2(la[:, 3] - la[:, 1], la[:, 2] - la[:, 0])
+    nags = radians < 0
+    radians[nags] += math.pi
+    radians = np.remainder(radians, math.pi)
+
+    for line in lines:
+        p1, p2 = map(Point2D, [(line[0], line[1]), (line[2], line[3])])
+        L1 = Line2D(p1, p2)
+        angle_raw = xaxis.angle_between(L1).evalf()
+        if angle_raw < 0:
+            angle_raw += math.pi
+        angle = angle_raw % math.pi
+        print(('angle ', math.degrees(angle)))
+        d = L1.length.evalf()
+        dd = d * d
+        y = int(line[1])
+        yhist[y][0] += 1
+        yhist[y][1] += d
+        yhist[y][2] += dd
+
+    return yhist
+
+
+def lines2quadrilaterals(lines, image_size):
+    def get_mid_point(line):
+        return (line[0] + line[2]) / 2, (line[1] + line[3]) / 2
+
+        # create a y histogram of lines
+
+    yhist = np.zeros((image_size[1], 3), dtype=float)
+    for line in lines:
+        L1 = Line2D(line)
+        yhist[line[0]] += 1
+        yhist[line[3]] += 1
+
+    cands = []
+    for s1, s2 in combinations(lines, 2):
+        # L1 = Line2D(s1)
+        # L2 = Line2D(s2)
+        # m1 = get_mid_point(s1)
+        # m2 = get_mid_point(s2)
+
+        points = [(s1[0], s1[1]), (s1[2], s1[3]), (s2[0], s2[1]), (s2[2], s2[3])]
+        poly = convex_hull(*points)
+        #        print(('check ', poly, poly.is_convex()))
+        passit = True
+        for other in lines:
+            if same_line(s1, other) or same_line(s2, other): continue
+            #            print(('against ', p1, p2))
+            if poly.encloses_point((other[0], other[1])) or poly.encloses_point((other[2], other[3])):
+                passit = False
+                #                print(('bad ', poly))
+                break
+        # print(('good ', poly))
+        if passit: cands.append(poly)
+    return cands
