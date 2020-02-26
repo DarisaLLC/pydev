79929362bb4c7c41f3a28e7ebf89dd3c3e3e52ec
gmnamra <gmnamra@gmail.com>
2020-02-26 10:18:20 -0800

    adjusted farnback motion parameters

--- lpolar.py
+++ lpolar.py
@@ -13,7 +13,7 @@ Keys:
 # Python 2/3 compatibility
 from __future__ import print_function
 
-import cv2 as cv
+import cv2
 
 if __name__ == '__main__':
     print(__doc__)
@@ -25,15 +25,18 @@ if __name__ == '__main__':
     except IndexError:
         fn = 'fruits.jpg'
 
-    img = cv.imread(fn)
+    img = cv2.imread(fn)
     if img is None:
         print('Failed to load image file:', fn)
         sys.exit(1)
 
-    img3 = cv.linearPolar(img, (314, 169), 20, cv.WARP_FILL_OUTLIERS + cv.INTER_LINEAR)
+    window = img[1000:1400,2000:2400]
+    img3 = cv2.linearPolar(window, (200,200), 200, cv2.WARP_FILL_OUTLIERS + cv2.INTER_LINEAR)
 
-    cv.imshow('before', img)
-    cv.imshow('linearpolar', img3)
-    cv.imwrite('/Users/arman/Desktop/linear.png', img3)
+    cv2.namedWindow('before', cv2.WINDOW_NORMAL)
+    cv2.imshow('before', window)
+    cv2.namedWindow('linearpolar', cv2.WINDOW_NORMAL)
+    cv2.imshow('linearpolar', img3)
+    cv2.imwrite('/Users/arman/Desktop/linear.png', img3)
 
-    cv.waitKey(0)
+    cv2.waitKey(0)
